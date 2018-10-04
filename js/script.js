bcf80b8def612010b09fc4f4d44971b176ca2978
Max Borschev <recrut2010@gmail.com>
2018-10-04 20:35:34 +0300

    Начал оживлять верстку
    Необходимо доделать кнопку для закрытия попап окна

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var link = document.querySelector(".write-us");
+
+var popup = document.querySelector(".modal-contacts");
+var close = popup.querySelector(".modal-close");
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+
+});
