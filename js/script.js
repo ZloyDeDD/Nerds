158ead638c463306549a49b2bbb828ed2f5cda1c
Max Borschev <recrut2010@gmail.com>
2018-10-07 15:01:01 +0300

    Оживил модальное окно и добавил яндекс карту

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ var popup = document.querySelector(".modal-contacts");
 var close = popup.querySelector(".modal-close");
 
 link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.add("modal-show");
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
 });
